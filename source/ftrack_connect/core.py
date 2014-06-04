b9f519f05dcde24c5ae032eca32cf4c79265c304
Carl Claesson <carl.claesson@ftrack.com>
2014-06-04 11:44:12 +0200

    Added bese connect error and renaming of methods.

--- source/ftrack_connect/core.py
+++ source/ftrack_connect/core.py
@@ -9,12 +9,19 @@ APPLICATION_ROOT = os.path.dirname(
     os.path.realpath(__file__)
 )
 
+# RESOURCEPATH environment variable is set by py2app/py2exe applications.
 RESOURCE_ROOT_PATH = os.path.join(
     os.environ.get(
         'RESOURCEPATH',
         APPLICATION_ROOT
     ), 'resources'
 )
+
+
+class ConnectError(Exception):
+    '''Base ftrack connect error.'''
+    pass
+
 
 class ApplicationWindow(QtGui.QMainWindow):
     '''Main window class for ftrack connect.'''
@@ -23,11 +30,7 @@ class ApplicationWindow(QtGui.QMainWindow):
         super(ApplicationWindow, self).__init__(*args, **kwargs)
 
         if not QtGui.QSystemTrayIcon.isSystemTrayAvailable():
-            QtGui.QMessageBox.critical(
-                None, 'Systray',
-                'I couldnt detect any system tray on this system.'
-            )
-            sys.exit(1)
+            raise ConnectError('No system tray located.')
 
         QtGui.QApplication.setQuitOnLastWindowClosed(False)
 
@@ -35,7 +38,7 @@ class ApplicationWindow(QtGui.QMainWindow):
             '{0}/logo.png'.format(RESOURCE_ROOT_PATH)
         )
 
-        self._initTray()
+        self._initialiseTray()
 
         self.setWindowTitle('ftrack connect')
         self.resize(300, 500)
@@ -43,9 +46,9 @@ class ApplicationWindow(QtGui.QMainWindow):
 
         self.setWindowIcon(self.logoIcon)
 
-    def _initTray(self):
+    def _initialiseTray(self):
         '''Initialise and add application icon to system tray.'''
-        self.trayMenu = self._initTrayMenu()
+        self.trayMenu = self._createTrayMenu()
 
         self.tray = QtGui.QSystemTrayIcon(self)
 
@@ -56,7 +59,7 @@ class ApplicationWindow(QtGui.QMainWindow):
         self.tray.setIcon(self.logoIcon)
         self.tray.show()
 
-    def _initTrayMenu(self):
+    def _createTrayMenu(self):
         '''Return a menu for system tray.'''
         menu = QtGui.QMenu(self)
 
