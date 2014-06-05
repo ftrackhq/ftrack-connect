a031714f1efdc07f3c3f346a36b1b7143a2cb4e6
Björn Rydahl <bjorn.rydahl@gmail.com>
2014-06-05 11:26:57 +0200

    Added linked to component.

--- source/ftrack_connect/publisher/core.py
+++ source/ftrack_connect/publisher/core.py
@@ -1,7 +1,18 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014 ftrack
 
-from PySide import QtGui
+import threading
+
+from PySide import QtGui, QtCore
+import ftrack
+
+
+def async(f):
+    '''Decorator to make a method asynchronous using its own thread.'''
+    def wrapper(*args, **kwargs):
+        thread = threading.Thread(target=f, args=args, kwargs=kwargs)
+        thread.start()
+    return wrapper
 
 
 def register(connect):
@@ -17,6 +28,8 @@ class Publisher(QtGui.QWidget):
         '''Return name of widget.'''
         return 'Publisher'
 
+    entityChanged = QtCore.Signal(object)
+
     def __init__(self, *args, **kwargs):
         '''Instantiate the publisher widget.'''
         super(Publisher, self).__init__(*args, **kwargs)
@@ -24,7 +37,48 @@ class Publisher(QtGui.QWidget):
             QtGui.QVBoxLayout()
         )
 
+        # Create form layout to keep track of publish form items.
+        layout = QtGui.QFormLayout()
+        self.layout().addLayout(layout)
+
+        # Add linked to component and connect to entitychange signal.
+        linkedTo = LinkedToComponent()
+        layout.addRow('Linked to', linkedTo)
+        self.entityChanged.connect(linkedTo.setEntity)
+
+        # TODO: Remove this call when it is possible to select or start
+        # publisher with an entity.
+        self.setEntity(ftrack.Task('d547547a-66de-11e1-bdb8-f23c91df25eb'))
+
     def addWidget(self, widget):
         '''Add *widget* to internal layout.'''
         layout = self.layout()
         layout.addWidget(widget)
+
+    def setEntity(self, entity):
+        '''Set the entity for the publisher.'''
+        self.entity = entity
+        self.entityChanged.emit(entity)
+
+
+class LinkedToComponent(QtGui.QLineEdit):
+    '''Linked to entity component.'''
+
+    def __init__(self, *args, **kwargs):
+        '''Instantiate the linked to component.'''
+        super(LinkedToComponent, self).__init__(*args, **kwargs)
+        self.setReadOnly(True)
+
+    @async
+    def setEntity(self, entity):
+        '''Set the entity for this component.'''
+        names = []
+        for parent in entity.getParents():
+            if isinstance(parent, ftrack.Show):
+                names.append(parent.getFullName())
+            else:
+                names.append(parent.getName())
+
+        # Reverse names since project should be first.
+        names.reverse()
+        self.setText(' / '.join(names))
