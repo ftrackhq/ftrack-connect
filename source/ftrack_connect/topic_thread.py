b3fc73549ea41511ed4ce69e289bc73ae162384b
Carl Claesson <carl.claesson@ftrack.com>
2014-06-11 15:10:56 +0200

    Drop all events triggered by other users.

--- source/ftrack_connect/topic_thread.py
+++ source/ftrack_connect/topic_thread.py
@@ -1,6 +1,8 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014 ftrack
 
+import os
+
 from PySide import QtCore
 import ftrack
 
@@ -11,13 +13,27 @@ class TopicThread(QtCore.QThread):
     '''Thread to subscribe to ftrack topics hub.'''
     ftrackConnectEvent = QtCore.Signal(object)
 
-    def _callback(self, topic, **data):
-        '''Generic callback for all ftrack.connect events.'''
-        self.ftrackConnectEvent.emit(
-            data.get('data', {})
-        )
+    def _callback(self, topic, _meta_, data):
+        '''Generic callback for all ftrack.connect events.
+
+        .. note::
+            Events not triggered by the current logged in user will be dropped.
+
+        '''
+        # Drop all events triggered by other users.
+        if not _meta_.get('userId') == self._currentUserId:
+            return
+
+        self.ftrackConnectEvent.emit(data)
 
     def run(self):
         '''Subscribe to ftrack.connect events and run the thread.'''
+
+        currentUser = ftrack.User(
+            os.environ['LOGNAME']
+        )
+
+        self._currentUserId = currentUser.getId()
+
         ftrack.TOPICS.subscribe('ftrack.connect', self._callback)
         ftrack.TOPICS.wait()
