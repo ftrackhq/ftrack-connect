6b31ccd90f321b2bd7768fdede8e9e65ca83be4c
Carl Claesson <carl.claesson@ftrack.com>
2014-06-12 14:15:48 +0200

    Updated topic thread docstring.

--- source/ftrack_connect/topic_thread.py
+++ source/ftrack_connect/topic_thread.py
@@ -8,7 +8,21 @@ import ftrack
 
 
 class TopicThread(QtCore.QThread):
-    '''Thread to subscribe to ftrack topics hub.'''
+    '''Thread to subscribe to ftrack topics hub.
+
+    The topic thread subscribes to all "ftrack.connect" events and emits a
+    signal with the event data.
+
+    .. code-block:: python
+
+        def _onFtrackConnectEvent(data):
+            # Do something with data
+
+        thread = TopicThread()
+        thread.ftrackConnectEvent.connect(_onFtrackConnectEvent)
+
+    '''
+
     ftrackConnectEvent = QtCore.Signal(object)
 
     def _callback(self, topic, _meta_, data):
