5ecb9441ce9fb1859f00b587651d54e0770cb964
Martin Pengelly-Phillips <martin@ftrack.com>
2014-07-15 20:53:12 +0100

    Refactor common test harness code to reusable class.

--- test/interactive/timer.py
+++ test/interactive/timer.py
@@ -1,78 +1,23 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014 ftrack
 
-import sys
-import argparse
-import logging
-
-from PySide import QtGui
-
-import ftrack_connect.ui.theme
 import ftrack_connect.ui.widget.timer
+from harness import Harness
 
 
-class TestHarness(QtGui.QDialog):
+class TestHarness(Harness):
     '''Test harness for widget.'''
 
-    def __init__(self, parent=None):
-        '''Initialise harness.'''
-        super(TestHarness, self).__init__(parent=parent)
-        self.setLayout(QtGui.QVBoxLayout())
-
-        self.timer = ftrack_connect.ui.widget.timer.Timer(
+    def constructWidget(self):
+        '''Return widget instance to test.'''
+        return ftrack_connect.ui.widget.timer.Timer(
             title='Compositing',
             description=('drones / sequence / a very very / long path / that '
                          'should be / elided / is-cu / station')
         )
-        self.layout().addWidget(self.timer)
-        self.layout().setSizeConstraint(QtGui.QLayout.SetMinAndMaxSize)
-
-
-def main(arguments=None):
-    '''Interactive test of timer widget.'''
-    if arguments is None:
-        arguments = []
-
-    parser = argparse.ArgumentParser()
-
-    # Allow setting of logging level from arguments.
-    loggingLevels = {}
-    for level in (
-        logging.NOTSET, logging.DEBUG, logging.INFO, logging.WARNING,
-        logging.ERROR, logging.CRITICAL
-    ):
-        loggingLevels[logging.getLevelName(level).lower()] = level
-
-    parser.add_argument(
-        '-v', '--verbosity',
-        help='Set the logging output verbosity.',
-        choices=loggingLevels.keys(),
-        default='info'
-    )
-    parser.add_argument(
-        '-t', '--theme',
-        help='Set the theme to use.',
-        choices=['light', 'dark'],
-        default='light'
-    )
-
-    namespace = parser.parse_args(arguments)
-
-    logging.basicConfig(level=loggingLevels[namespace.verbosity])
-
-    # Construct global application.
-    application = QtGui.QApplication(arguments)
-
-    # Construct test harness and apply theme.
-    dialog = TestHarness()
-    ftrack_connect.ui.theme.applyTheme(dialog, namespace.theme)
-
-    dialog.show()
-
-    return application.exec_()
 
 
 if __name__ == '__main__':
     raise SystemExit(
-        main(sys.argv[1:])
+        TestHarness().run()
     )
