c3159c550d97556a55979780cfb32975f3efcf17
Martin Pengelly-Phillips <martin@ftrack.com>
2014-07-15 23:20:19 +0100

    Add ability to manually set time in interactive test.

--- test/interactive/timer.py
+++ test/interactive/timer.py
@@ -1,6 +1,8 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014 ftrack
 
+from PySide import QtGui
+
 import ftrack_connect.ui.widget.timer
 from harness import Harness
 
@@ -10,11 +12,40 @@ class WidgetHarness(Harness):
 
     def constructWidget(self):
         '''Return widget instance to test.'''
-        return ftrack_connect.ui.widget.timer.Timer(
+        widget = QtGui.QWidget()
+        layout = QtGui.QVBoxLayout()
+        widget.setLayout(layout)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self._timer = timer = ftrack_connect.ui.widget.timer.Timer(
             title='Compositing',
             description=('drones / sequence / a very very / long path / that '
                          'should be / elided / is-cu / station')
         )
+        layout.addWidget(timer, stretch=0)
+
+        divider = QtGui.QFrame()
+        divider.setFrameShape(QtGui.QFrame.HLine)
+        layout.addWidget(divider)
+
+        driverLayout = QtGui.QHBoxLayout()
+        layout.addLayout(driverLayout)
+
+        self._timeInput = timeInput = QtGui.QSpinBox()
+        timeInput.setRange(0, 999 * 60 * 60)
+        driverLayout.addWidget(timeInput)
+
+        applyButton = QtGui.QPushButton('Apply')
+        driverLayout.addWidget(applyButton)
+
+        applyButton.clicked.connect(self.setTime)
+
+        return widget
+
+    def setTime(self):
+        '''Set time.'''
+        time = self._timeInput.value()
+        self._timer.setTime(time)
 
 
 if __name__ == '__main__':
