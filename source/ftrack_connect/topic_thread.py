667416f2d31fa898252edc46411b4a4331a6541e
Mattias Lagergren <mattias.lagergren@gmail.com>
2014-07-16 16:14:22 +0200

    Fixed docstring for TopicThread class

--- source/ftrack_connect/topic_thread.py
+++ source/ftrack_connect/topic_thread.py
@@ -1,27 +1,12 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014 ftrack
 
-import getpass
-
 from PySide import QtCore
 import ftrack
 
 
 class TopicThread(QtCore.QThread):
-    '''Thread to subscribe to ftrack topics hub.
-
-    The topic thread subscribes to all "ftrack.connect" events and emits a
-    signal with the event data.
-
-    .. code-block:: python
-
-        def _onFtrackConnectEvent(data):
-            # Do something with data
-
-        thread = TopicThread()
-        thread.ftrackConnectEvent.connect(_onFtrackConnectEvent)
-
-    '''
+    '''Thread to listen on ftrack's topics hub.'''
 
     def run(self):
         '''Run the topic thread.'''
