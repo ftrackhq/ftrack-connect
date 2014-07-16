3520e973ce6c90b131f16cbb5897505001938d24
Martin Pengelly-Phillips <martin@ftrack.com>
2014-07-16 10:15:12 +0100

    Fix tab text being obscured on windows.

--- source/ftrack_connect/ui/time_logger.py
+++ source/ftrack_connect/ui/time_logger.py
@@ -1,13 +1,19 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014 ftrack
 
+from PySide import QtGui
+
 import ftrack_connect.ui.application
+import ftrack_connect.ui.widget.timer
 
 
 def register(connect):
     '''Register publish plugin to ftrack connect.'''
     timeLogger = TimeLogger()
     connect.addPlugin(timeLogger)
+
+    # TEMP: Dev only.
+    timeLogger.requestApplicationFocus.emit(timeLogger)
 
 
 class TimeLogger(ftrack_connect.ui.application.ApplicationPlugin):
@@ -16,6 +22,13 @@ class TimeLogger(ftrack_connect.ui.application.ApplicationPlugin):
     def __init__(self, *args, **kwargs):
         '''Instantiate the time logger.'''
         super(TimeLogger, self).__init__(*args, **kwargs)
+        layout = QtGui.QVBoxLayout()
+        self.setLayout(layout)
+
+        self.timer = ftrack_connect.ui.widget.timer.Timer()
+        layout.addWidget(self.timer)
+
+        layout.addStretch(1)
 
     def getName(self):
         '''Return name of widget.'''
