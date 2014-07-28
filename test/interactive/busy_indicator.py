8305375b65f552dcdf57287c74869b039e806518
Martin Pengelly-Phillips <martin@ftrack.com>
2014-07-28 21:18:19 +0100

    Add scalable busy indicator widget.

---
+++ test/interactive/busy_indicator.py
@@ -0,0 +1,30 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2014 ftrack
+
+from PySide import QtGui, QtCore
+
+import ftrack_connect.ui.widget.indicator
+from harness import Harness
+
+
+class WidgetHarness(Harness):
+    '''Test harness for widget.'''
+
+    def constructWidget(self):
+        '''Return widget instance to test.'''
+        widget = QtGui.QFrame()
+        widget.setFrameStyle(QtGui.QFrame.StyledPanel | QtGui.QFrame.Plain)
+        widget.setStyleSheet('QFrame { background-color: #555; }')
+        widget.setLayout(QtGui.QVBoxLayout())
+
+        self._indicator = ftrack_connect.ui.widget.indicator.BusyIndicator()
+        self._indicator.setMinimumSize(100, 100)
+
+        widget.layout().addWidget(self._indicator)
+        return widget
+
+
+if __name__ == '__main__':
+    raise SystemExit(
+        WidgetHarness().run()
+    )
