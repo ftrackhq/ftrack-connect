0558d4737fc609741d0ba1ba054075a37bcc1641
Martin Pengelly-Phillips <martin@ftrack.com>
2014-07-29 10:53:44 +0100

    Standardise interactive test controls.

--- test/interactive/busy_indicator.py
+++ test/interactive/busy_indicator.py
@@ -1,7 +1,7 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014 ftrack
 
-from PySide import QtGui, QtCore
+from PySide import QtGui
 
 import ftrack_connect.ui.widget.indicator
 from harness import Harness
@@ -12,16 +12,27 @@ class WidgetHarness(Harness):
 
     def constructWidget(self):
         '''Return widget instance to test.'''
-        widget = QtGui.QFrame()
-        widget.setFrameStyle(QtGui.QFrame.StyledPanel | QtGui.QFrame.Plain)
-        widget.setStyleSheet('QFrame { background-color: #efefef; }')
-        widget.setLayout(QtGui.QVBoxLayout())
+        indicator = ftrack_connect.ui.widget.indicator.BusyIndicator()
+        indicator.setMinimumSize(100, 100)
 
-        self._indicator = ftrack_connect.ui.widget.indicator.BusyIndicator()
-        self._indicator.setMinimumSize(100, 100)
+        return indicator
 
-        widget.layout().addWidget(self._indicator)
-        return widget
+    def constructController(self, widget):
+        '''Return controller for *widget*.'''
+        controlWidget = QtGui.QWidget()
+        layout = QtGui.QHBoxLayout()
+        controlWidget.setLayout(layout)
+
+        startButton = QtGui.QPushButton('Start')
+        layout.addWidget(startButton)
+
+        stopButton = QtGui.QPushButton('Stop')
+        layout.addWidget(stopButton)
+
+        startButton.clicked.connect(widget.start)
+        stopButton.clicked.connect(widget.stop)
+
+        return controlWidget
 
 
 if __name__ == '__main__':
