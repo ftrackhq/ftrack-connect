e23566ccc2e896b1598bc04a0188bc8b96384288
Martin Pengelly-Phillips <martin@ftrack.com>
2014-09-09 15:47:43 +0100

    Move third-party imports to be above local ones.
    Also, makes sense as it then reads better that resources depending on those libraries are loaded afterwards.

--- source/ftrack_connect/ui/__init__.py
+++ source/ftrack_connect/ui/__init__.py
@@ -1,10 +1,10 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014 ftrack
 
-# Load UI resources such as icons.
-from . import resource as _resource
-
 # Import QtSvg and QtXml to force load libraries needed to display
-# SVG on Windows. 
+# SVG on Windows.
 from PySide import QtSvg
 from PySide import QtXml
+
+# Load UI resources such as icons.
+from . import resource as _resource
