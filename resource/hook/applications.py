7ab02d25199869470965bed6a0aa2464280539c6
Martin Pengelly-Phillips <martin@ftrack.com>
2014-12-23 12:18:59 +0000

    Refactor event specific code as just a hook.
    Application discovery library code is already provided by ApplicationStore.

--- resource/hook/applications.py
+++ resource/hook/applications.py
@@ -45,20 +45,118 @@ class LaunchApplicationHook(object):
         )
 
 
+class DiscoverApplicationsHook(object):
+    '''Default action.discover hook.
+
+    The class is callable and return an object with a list of actions that can
+    be launched on this computer.
+
+    Example:
+
+        dict(
+            items=[
+                dict(
+                    actionIdentifier='ftrack-connect-launch-applications-action',
+                    label='Maya 2014',
+                    actionData=dict(
+                        applicationIdentifier='maya_2014'
+                    )
+                ),
+                dict(
+                    actionIdentifier='ftrack-connect-launch-applications-action',
+                    label='Premiere Pro CC 2014',
+                    actionData=dict(
+                        applicationIdentifier='pp_cc_2014'
+                    )
+                ),
+                dict(
+                    actionIdentifier='ftrack-connect-launch-applications-action',
+                    label='Premiere Pro CC 2014 with latest publish',
+                    actionData=dict(
+                        latest=True,
+                        applicationIdentifier='pp_cc_2014'
+                    )
+                )
+            ]
+        )
+
+    '''
+
+    identifier = ACTION_IDENTIFIER
+
+    def __init__(self, applicationStore):
+        '''Instantiate the hook and setup logging.'''
+        super(DiscoverApplicationsHook, self).__init__()
+        self.logger = logging.getLogger(
+            __name__ + '.' + self.__class__.__name__
+        )
+        self.applicationStore = applicationStore
+
+    def __call__(self, event):
+        '''Default action.discover hook.
+
+        The hook callback accepts an *event*.
+
+        event['data'] should contain:
+
+            context - Context of request to help guide what applications can be
+                      launched.
+
+        '''
+        context = event['data']['context']
+
+        # If selection contains more than one item return early since
+        # applications cannot be started for multiple items or if the
+        # selected item is not a "task".
+        selection = context.get('selection', [])
+        if len(selection) != 1 or selection[0].get('entityType') != 'task':
+            return
+
+        items = []
+        applications = self.applicationStore.applications
+        applications = sorted(
+            applications, key=lambda application: application['label']
+        )
+
+        for application in applications:
+            applicationIdentifier = application['identifier']
+            label = application['label']
+            items.append({
+                'actionIdentifier': self.identifier,
+                'label': label,
+                'icon': application.get('icon', 'default'),
+                'actionData': {
+                    'applicationIdentifier': applicationIdentifier
+                }
+            })
+
+            if applicationIdentifier.startswith('premiere_pro_cc'):
+                items.append({
+                    'actionIdentifier': self.identifier,
+                    'label': '{label} with latest version'.format(
+                        label=label
+                    ),
+                    'icon': application.get('icon', 'default'),
+                    'actionData': {
+                        'launchWithLatest': True,
+                        'applicationIdentifier': applicationIdentifier
+                    }
+                })
+
+        return {
+            'items': items
+        }
+
+
 def register(registry, **kw):
     '''Register hooks.'''
     applicationStore = ftrack_connect.application.ApplicationStore()
-    applicationLauncher = ftrack_connect.application.ApplicationLauncher(
-        applicationStore
-    )
 
     ftrack.EVENT_HUB.subscribe(
         'topic=ftrack.action.discover and source.user.username={0}'.format(
             getpass.getuser()
         ),
-        ftrack_connect.application.ApplicationDiscoverer(
-            applicationStore, ACTION_IDENTIFIER
-        )
+        DiscoverApplicationsHook(applicationStore)
     )
 
     ftrack.EVENT_HUB.subscribe(
@@ -66,5 +164,9 @@ def register(registry, **kw):
         'and data.actionIdentifier={1}'.format(
             getpass.getuser(), ACTION_IDENTIFIER
         ),
-        LaunchApplicationHook(applicationLauncher)
+        LaunchApplicationHook(
+            ftrack_connect.application.ApplicationLauncher(
+                applicationStore
+            )
+        )
     )
