c03d81dd3b51c91b4bf8cccfc9e684ced6246b32
Mattias Lagergren <mattias.lagergren@ftrack.com>
2015-02-26 11:12:47 +0100

    Fixed thumbnail code style

--- source/ftrack_connect/ui/widget/thumbnail.py
+++ source/ftrack_connect/ui/widget/thumbnail.py
@@ -3,7 +3,9 @@ import os
 import urllib2
 from PySide import QtGui, QtCore
 
+
 class Thumbnail(QtGui.QLabel):
+    '''Widget to load thumbnails from ftrack server.'''
 
     def __init__(self, parent=None):
         super(Thumbnail, self).__init__(parent)
@@ -15,7 +17,20 @@ class Thumbnail(QtGui.QLabel):
         self.placholderThumbnail = (os.environ['FTRACK_SERVER']
                                     + '/img/thumbnail2.png')
 
-    def getFileFromUrl(self, url):
+    def loadFromUrl(self, url):
+        '''Load thumbnail from *url* and display it.'''
+        self.setText('')
+        if url is None:
+            url = self.placholderThumbnail
+        pixmap = self._pixmapFromUrl(url)
+        scaledPixmap = pixmap.scaledToWidth(
+            self.width(),
+            mode=QtCore.Qt.SmoothTransformation
+        )
+        self.setPixmap(scaledPixmap)
+
+    def _getFileFromUrl(self, url):
+        '''Return thumbnail file from *url*.'''
         ftrackProxy = os.getenv('FTRACK_PROXY', '')
         ftrackServer = os.getenv('FTRACK_SERVER', '')
         if ftrackProxy != '':
@@ -34,29 +49,17 @@ class Thumbnail(QtGui.QLabel):
 
         return html
 
-    def loadFromUrl(self, url):
-        '''Update thumbnail for *label* with image at *url*.'''
-        self.setText('')
-        if url is None:
-            url = self.placholderThumbnail
-        pixmap = self._pixmapFromUrl(url)
-        scaledPixmap = pixmap.scaledToWidth(
-            self.width(),
-            mode=QtCore.Qt.SmoothTransformation
-        )
-        self.setPixmap(scaledPixmap)
-
     def _pixmapFromUrl(self, url):
         '''Retrieve *url* and return data as a pixmap.'''
         pixmap = self.thumbnailCache.get(url)
         if pixmap is None:
-            data = self.getFileFromUrl(url)
+            data = self._getFileFromUrl(url)
             pixmap = QtGui.QPixmap()
             pixmap.loadFromData(data)
             self.thumbnailCache[url] = pixmap
-        
+
         # Handle null pixmaps. E.g. JPG on Windows.
         if pixmap.isNull():
             pixmap = self.thumbnailCache.get(self.placholderThumbnail, pixmap)
-            
+
         return pixmap
