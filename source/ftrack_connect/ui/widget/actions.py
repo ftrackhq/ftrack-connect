c19c937f63dfea01c313a3fa0efce33a92440b82
Lucas Correia <lucas.correia@ftrack.com>
2015-09-08 09:56:59 +0200

    Improve empty state for actions tab

--- source/ftrack_connect/ui/widget/actions.py
+++ source/ftrack_connect/ui/widget/actions.py
@@ -64,18 +64,21 @@ class Actions(QtGui.QWidget):
         self._entitySelector.entityChanged.connect(
             self._onEntityChanged
         )
-        layout.addWidget(QtGui.QLabel('Context'))
+        layout.addWidget(QtGui.QLabel('Select action context'))
         layout.addWidget(self._entitySelector)
 
+        self._recentLabel = QtGui.QLabel('Recent')
+        layout.addWidget(self._recentLabel)
         self._recentSection = ActionSection(self)
         self._recentSection.setFixedHeight(100)
         self._recentSection.launchedAction.connect(self._onActionLaunched)
-        layout.addWidget(QtGui.QLabel('Recent'))
         layout.addWidget(self._recentSection)
 
+        self._allLabel = QtGui.QLabel('Discovering actions..')
+        self._allLabel.setAlignment(QtCore.Qt.AlignCenter)
+        layout.addWidget(self._allLabel)
         self._allSection = ActionSection(self)
         self._allSection.launchedAction.connect(self._onActionLaunched)
-        layout.addWidget(QtGui.QLabel('All'))
         layout.addWidget(self._allSection)
 
         self._loadActionsForContext([])
@@ -112,12 +115,27 @@ class Actions(QtGui.QWidget):
                 if action[0]['label'] == recentAction:
                     recentActions.append(action)
 
-        self._recentSection.addActions(recentActions)
+        if recentActions:
+            self._recentSection.addActions(recentActions)
+            self._recentLabel.show()
+            self._recentSection.show()
+        else:
+            self._recentLabel.hide()
+            self._recentSection.hide()
 
     def _updateAllSection(self):
         '''Clear and update actions in all section.'''
         self._allSection.clear()
-        self._allSection.addActions(self._actions)
+        if self._actions:
+            self._allSection.addActions(self._actions)
+            self._allLabel.setAlignment(QtCore.Qt.AlignLeft)
+            self._allLabel.setText('All actions')
+        else:
+            self._allLabel.setAlignment(QtCore.Qt.AlignCenter)
+            self._allLabel.setText(
+                '<h2 style="font-weight: normal">No actions found</h2>'
+                '<p>Try another selection or add some actions.</p>'
+            )
 
     @ftrack_connect.asynchronous.asynchronous
     def _updateRecentActions(self):
