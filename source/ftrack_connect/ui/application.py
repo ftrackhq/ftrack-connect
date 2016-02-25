e0c63b6ef398457adfa38ec0881904bc21f45d9f
Björn Rydahl <bjorn.rydahl@gmail.com>
2016-02-25 18:22:48 +0100

    Moved login tools into their own file.

--- source/ftrack_connect/ui/application.py
+++ source/ftrack_connect/ui/application.py
@@ -5,10 +5,6 @@ import os
 import getpass
 import platform
 import logging
-import BaseHTTPServer
-import urlparse
-import webbrowser
-import functools
 import requests
 
 from PySide import QtGui
@@ -24,79 +20,7 @@ from ftrack_connect.ui.widget import tab_widget as _tab_widget
 from ftrack_connect.ui.widget import login as _login
 from ftrack_connect.ui.widget import about as _about
 from ftrack_connect.error import NotUniqueError as _NotUniqueError
-
-
-class LoginServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    '''Login server handler.'''
-
-    def __init__(self, login_callback, *args, **kw):
-        '''Initialise handler.'''
-        self.login_callback = login_callback
-        BaseHTTPServer.BaseHTTPRequestHandler.__init__(self, *args, **kw)
-
-    def do_GET(self):
-        '''Override to handle requests ourselves.'''
-        parsed_path = urlparse.urlparse(self.path)
-        query = parsed_path.query
-
-        login_credentials = None
-        if 'api_user' and 'api_key' in query:
-            login_credentials = urlparse.parse_qs(query)
-            message = """
-                <html>
-                <body>
-                  <center>
-                  <div style="margin:auto; margin-top:30px; max-width:350px">
-                <p>Sign in to ftrack connect was successful.</p>
-                    <p style="color:#aaaaaa">You can now close this window.</p>
-                </div>
-                  </center>
-                </body>
-                </html>
-            """
-        else:
-            message = 'Empty page.'
-
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(message)
-
-        if login_credentials:
-            self.login_callback(
-                login_credentials['api_user'][0],
-                login_credentials['api_key'][0]
-            )
-
-
-class LoginServerThread(QtCore.QThread):
-    '''Login server thread.'''
-
-    # Login signal.
-    loginSignal = QtCore.Signal(object, object, object)
-
-    def start(self, url):
-        '''Start thread.'''
-        self.url = url
-        super(LoginServerThread, self).start()
-
-    def _handle_login(self, api_user, api_key):
-        '''Login to server with *api_user* and *api_key*.'''
-        self.loginSignal.emit(self.url, api_user, api_key)
-
-    def run(self):
-        '''Listen for events.'''
-        self._server = BaseHTTPServer.HTTPServer(
-            ('localhost', 0),
-            functools.partial(
-                LoginServerHandler, self._handle_login
-            )
-        )
-        webbrowser.open_new_tab(
-            '{0}/user/api_credentials?redirect_url=http://localhost:{1}'.format(
-                self.url, self._server.server_port
-            )
-        )
-        self._server.handle_request()
+from ftrack_connect.ui import login_tools as _login_tools
 
 
 class ApplicationPlugin(QtGui.QWidget):
@@ -298,7 +222,7 @@ class Application(QtGui.QMainWindow):
         # If credentials are not properly set, try to get them using a http
         # server.
         if not username or not apiKey:
-            self._login_server_thread = LoginServerThread()
+            self._login_server_thread = _login_tools.LoginServerThread()
             self._login_server_thread.loginSignal.connect(self.loginSignal)
             self._login_server_thread.start(url)
             return
