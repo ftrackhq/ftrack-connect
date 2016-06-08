bb74010e9f11357144100071bb0dcc348efb94e2
Björn Rydahl <bjorn.rydahl@ftrack.com>
2016-06-08 11:17:07 +0200

    Updated to catch errors. Added tracking of two new events.

--- source/ftrack_connect/usage.py
+++ source/ftrack_connect/usage.py
@@ -1,26 +1,32 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2016 ftrack
 
+import logging
+
 import ftrack_connect.asynchronous
 import ftrack_connect.session
 
 
+logger = logging.getLogger('ftrack_connect:usage')
 _log_usage_session = None
 
 
 @ftrack_connect.asynchronous.asynchronous
-def log(event_name, metadata=None):
-    '''Log usage with *event_name* and *metadata*.'''
+def send_event(event_name, metadata=None):
+    '''Send usage event with *event_name* and *metadata*.'''
     global _log_usage_session
 
     if _log_usage_session is None:
         _log_usage_session = ftrack_connect.session.get_session()
 
-    _log_usage_session._call([{
-        'action': '_track_usage',
-        'data': {
-            'type': 'event',
-            'name': event_name,
-            'data': metadata
-        }
-    }])
+    try:
+        _log_usage_session._call([{
+            'action': '_track_usage',
+            'data': {
+                'type': 'event',
+                'name': event_name,
+                'metadata': metadata
+            }
+        }])
+    except Exception:
+        logger.exception('Failed to send event.')
