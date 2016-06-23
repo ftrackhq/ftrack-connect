4666864486714d1eab9c4a6dd7d0ef2b87107801
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2016-06-23 10:07:23 +0200

    move from warning to info

--- source/ftrack_connect/config.py
+++ source/ftrack_connect/config.py
@@ -92,4 +92,4 @@ def configure_logging(logger_name, level=None, format=None):
     logging.captureWarnings(True)
 
     # Log out the file output.
-    logging.warning('Saving log file to: {0}'.format(logfile))
+    logging.info('Saving log file to: {0}'.format(logfile))
