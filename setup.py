712a406df99f944e22387c398b0b28d866e424e1
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2016-12-20 10:50:32 +0100

    add ftrack-location-compatibility as dependency to connect

--- setup.py
+++ setup.py
@@ -236,7 +236,8 @@ configuration = dict(
         'PySide >= 1.2.2, < 2',
         'sphinx >= 1.2.2, < 2',
         'sphinx_rtd_theme >= 0.1.6, < 2',
-        'lowdown >= 0.1.0, < 1'
+        'lowdown >= 0.1.0, < 1',
+        'ftrack-location-compatibility'
     ],
     install_requires=[
         'qtext',
@@ -245,7 +246,8 @@ configuration = dict(
         'Riffle',
         'arrow >= 0.4.6, < 1',
         'appdirs == 1.4.0',
-        'requests >= 2, <3'
+        'requests >= 2, <3',
+        'ftrack-location-compatibility'
     ],
     tests_require=['pytest >= 2.3.5, < 3'],
     cmdclass={
@@ -263,6 +265,10 @@ configuration = dict(
         (
             'git+https://bitbucket.org/ftrack/qtext/get/0.1.0.zip'
             '#egg=QtExt-0.1.0'
+        ),
+        (
+            'git+https://bitbucket.org/ftrack/ftrack-location-compatibility/get/0.1.0.zip'
+            '#egg=ftrack-location-compatibility-0.1.0'
         )
     ],
     options={},
