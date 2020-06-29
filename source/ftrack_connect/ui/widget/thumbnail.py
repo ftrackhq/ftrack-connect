e7ff05e2821962d6fd7c929cdc4ebb7291fac153
lorenzo <lorenzo.angeli@ftrack.com>
2020-06-29 15:28:15 +0200

    ensure url is unicode, this seems to stop connect from crashing

--- source/ftrack_connect/ui/widget/thumbnail.py
+++ source/ftrack_connect/ui/widget/thumbnail.py
@@ -152,7 +152,7 @@ class ActionIcon(Base):
             self.load(icon)
 
         elif self.AVAILABLE_ICONS.get(icon):
-            url = os.environ['FTRACK_SERVER'] + self.AVAILABLE_ICONS[icon]
+            url = unicode(os.environ['FTRACK_SERVER'] + self.AVAILABLE_ICONS[icon], 'utf-8')
             self.load(url)
 
         else:
