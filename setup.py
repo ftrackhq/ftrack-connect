3eafc2700373543e9055c8b994dbbb1b5b2169a0
lorenzo <lorenzo.angeli@ftrack.com>
2020-10-19 14:40:31 +0200

    provide both PR for testing

--- setup.py
+++ setup.py
@@ -270,7 +270,9 @@ configuration = dict(
         'Qt.py >=1.0.0, < 2',
     ],
     dependency_links=[
-        'git+https://bitbucket.org/kalemas/ftrack-python-api.git@wip/thread-local-auto-populate#egg=ftrack-python-api-2.0.0'
+        # 'git+https://bitbucket.org/kalemas/ftrack-python-api.git@wip/thread-local-auto-populate#egg=ftrack-python-api-2.0.0',
+        'git+https://bitbucket.org/kalemas/ftrack-python-api.git@wip-guard-with-rlock#egg=ftrack-python-api-2.0.0',
+
     ],
     tests_require=[
         'pytest >= 2.3.5, < 3'
