cd1bdccfb5254a930f706a8fc36557be0666825f
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2021-11-12 15:05:25 +0100

    add module and helper methods back

---
+++ source/ftrack_connect/application.py
@@ -0,0 +1,31 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2014 ftrack
+
+import os
+
+def prependPath(path, key, environment):
+    '''Prepend *path* to *key* in *environment*.'''
+    try:
+        environment[key] = (
+            os.pathsep.join([
+                path, environment[key]
+            ])
+        )
+    except KeyError:
+        environment[key] = path
+
+    return environment
+
+
+def appendPath(path, key, environment):
+    '''Append *path* to *key* in *environment*.'''
+    try:
+        environment[key] = (
+            os.pathsep.join([
+                environment[key], path
+            ])
+        )
+    except KeyError:
+        environment[key] = path
+
+    return environment
