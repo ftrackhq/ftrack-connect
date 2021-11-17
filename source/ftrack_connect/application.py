f838a0d93ae93acfabb08e78dd1f0a7947f84500
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2021-11-17 16:19:42 +0100

    provide wrapper with deprecation warnings and set new methods to new syntax

--- source/ftrack_connect/application.py
+++ source/ftrack_connect/application.py
@@ -2,8 +2,10 @@
 # :copyright: Copyright (c) 2014 ftrack
 
 import os
+import warnings
 
-def prependPath(path, key, environment):
+
+def prepend_path(path, key, environment):
     '''Prepend *path* to *key* in *environment*.'''
     try:
         environment[key] = (
@@ -17,7 +19,7 @@ def prependPath(path, key, environment):
     return environment
 
 
-def appendPath(path, key, environment):
+def append_path(path, key, environment):
     '''Append *path* to *key* in *environment*.'''
     try:
         environment[key] = (
@@ -29,3 +31,22 @@ def appendPath(path, key, environment):
         environment[key] = path
 
     return environment
+
+
+# legacy 
+def appendPath(path, key, environment):
+    '''Prepend *path* to *key* in *environment*.'''
+    warnings.warn(
+        "appendPath will be deprecated in upcoming version, use append_path instead",
+        PendingDeprecationWarning
+    )
+    return append_path(path, key, environment)
+
+
+def prependPath(path, key, environment):
+    '''Prepend *path* to *key* in *environment*.'''
+    warnings.warn(
+        "prependPath will be deprecated in upcoming version, use prepend_path instead",
+        PendingDeprecationWarning
+    )
+    return prepend_path(path, key, environment)
