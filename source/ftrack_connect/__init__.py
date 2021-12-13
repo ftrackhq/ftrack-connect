48e9709f3b8b185197902b4c121fc812a04433d4
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2021-12-13 12:34:32 +0100

    rework setup to install local fonts add ftrack-icon.ttf and -charmap.json for qtawesome

--- source/ftrack_connect/__init__.py
+++ source/ftrack_connect/__init__.py
@@ -2,3 +2,11 @@
 # :copyright: Copyright (c) 2014 ftrack
 
 from ._version import __version__
+
+import qtawesome
+
+fonts = qtawesome.load_font(
+    'ftrack',
+    'ftrack-icon.ttf',
+    'ftrack-icon-charmap.json'
+)
