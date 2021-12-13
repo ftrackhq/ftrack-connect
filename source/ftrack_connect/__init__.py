a656f853b2aa0938ed5e8bc4c32e5a68c32c92b9
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2021-12-13 14:17:03 +0100

    load icons

--- source/ftrack_connect/__init__.py
+++ source/ftrack_connect/__init__.py
@@ -1,12 +1,20 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014 ftrack
 
-from ._version import __version__
-
+import os
 import qtawesome
 
-fonts = qtawesome.load_font(
-    'ftrack',
-    'ftrack-icon.ttf',
-    'ftrack-icon-charmap.json'
-)
+from ._version import __version__
+
+
+
+def load_icons(font_folder):
+    font_folder = os.path.abspath(font_folder)
+    print('loading icons from :{} '.format(font_folder))
+
+    qtawesome.load_font(
+        'ftrack',
+        'ftrack-icon.ttf',
+        'ftrack-icon-charmap.json',
+        directory=font_folder
+    )
--- source/ftrack_connect/ui/__init__.py
+++ source/ftrack_connect/ui/__init__.py
@@ -3,9 +3,14 @@
 
 # Import QtSvg and QtXml to force load libraries needed to display
 # SVG on Windows.
+import os
 from Qt import QtSvg
 from Qt import QtXml
 
 
 # Load UI resources such as icons.
 from . import resource as _resource
+from ftrack_connect import load_icons
+
+fonts = os.path.join(os.path.dirname(__file__), '..', 'fonts')
+load_icons(fonts)
