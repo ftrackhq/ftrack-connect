c8bc58e00363d2a9ca4a4f0f9227901470d12577
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2021-12-16 12:39:29 +0100

    set 1/3 of widget size for cloud icon

--- source/ftrack_connect/ui/widget/configure_scenario.py
+++ source/ftrack_connect/ui/widget/configure_scenario.py
@@ -42,7 +42,7 @@ class ConfigureScenario(QtWidgets.QWidget):
         cloud_icon = qta.icon('ftrack.cloud-done')
         icon = QtWidgets.QLabel()
         icon.setPixmap(
-            cloud_icon.pixmap(QtCore.QSize(self.width(), self.height()))
+            cloud_icon.pixmap(QtCore.QSize(self.width()/3, self.height()/3))
         )
 
         icon.setAlignment(QtCore.Qt.AlignCenter)
