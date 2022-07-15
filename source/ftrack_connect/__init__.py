816db09b04fdf634b7ff9858d3e1b6e84281be6d
lorenzo angeli <hdd@users.noreply.github.com>
2022-07-15 12:56:10 +0200

    ensure icons are loaded within the code and not before the module

--- source/ftrack_connect/__init__.py
+++ source/ftrack_connect/__init__.py
@@ -2,13 +2,17 @@
 # :copyright: Copyright (c) 2014 ftrack
 
 import os
+import logging
 import qtawesome as qta
 import darkdetect
 from ._version import __version__
 
+logger = logging.getLogger(__name__)
+
 
 def load_icons(font_folder):
     font_folder = os.path.abspath(font_folder)
+    logger.info(f'loading ftrack icon fonts from {font_folder}')
     qta.load_font(
         'ftrack',
         'ftrack-icon.ttf',
